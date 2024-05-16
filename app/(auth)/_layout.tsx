cc3e13ba2d549b8346d4f755a08efa04de8cf39e
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-05-16 11:45:41 +0700

    auth-login login screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,16 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="register" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="dark" />
+    </>
   );
 };
 
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,3 +1,5 @@
 import user from "@/assets/icons/user.png";
+import eye from "@/assets/icons/eye.png";
+import eyeOff from "@/assets/icons/eye-off.png";
 
-export default { user };
+export default { user, eye, eyeOff };
