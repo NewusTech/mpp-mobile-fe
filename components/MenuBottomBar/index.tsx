c37fbf6af216f70e747158363833663869d54450
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-05-27 15:23:41 +0700

    chat screen

--- components/MenuBottomBar/index.tsx
+++ components/MenuBottomBar/index.tsx
@@ -2,16 +2,19 @@ import { Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import Gap from "../Gap";
 import { BottombarProps } from "@/types/type";
+import { Link } from "expo-router";
 
-const MenuBottomBar = ({ icon, title }: BottombarProps) => {
+const MenuBottomBar = ({ icon, title, route }: BottombarProps) => {
   return (
-    <TouchableOpacity className="flex items-center justify-center">
-      <Image source={icon} className="w-[25px] h-[25px]" />
-      <Gap height={8} />
-      <Text className="text-[8px] text-neutral-50 w-[48px] text-center">
-        {title}
-      </Text>
-    </TouchableOpacity>
+    <Link href={route} asChild>
+      <TouchableOpacity className="flex items-center justify-center">
+        <Image source={icon} className="w-[25px] h-[25px]" />
+        <Gap height={8} />
+        <Text className="text-[8px] text-neutral-50 w-[48px] text-center">
+          {title}
+        </Text>
+      </TouchableOpacity>
+    </Link>
   );
 };
 
